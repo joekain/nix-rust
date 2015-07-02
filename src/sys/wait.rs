68c24979f26f23258feedb84825c90867b07bd15
Joseph Kain <joekain@gmail.com>
2015-07-01 22:03:07 -0700

    Make status available from waitpid.
    * Extend the enums in WaitStatus to include all process states (signaled,
      stopped, exited, continued).
    * Decode status from waitpid
    * Return appropate WaitStatus
    * Update tests to use the new WaitStatus

--- src/sys/wait.rs
+++ src/sys/wait.rs
@@ -2,6 +2,8 @@ use libc::{pid_t, c_int};
 use errno::Errno;
 use {Error, Result};
 
+use sys::signal;
+
 mod ffi {
     use libc::{pid_t, c_int};
 
@@ -18,8 +20,63 @@ bitflags!(
 
 #[derive(Eq, PartialEq, Clone, Copy, Debug)]
 pub enum WaitStatus {
-    Exited(pid_t),
+    Exited(pid_t, i8),
+    Signaled(pid_t, signal::SigNum, bool),
+    Stopped(pid_t, signal::SigNum),
+    Continued(pid_t),
     StillAlive
+}
+
+mod status {
+    use libc::pid_t;
+    use super::WaitStatus;
+    use sys::signal;
+
+    fn exited(status: i32) -> bool {
+        (status & 0x7F) == 0
+    }
+
+    fn exit_status(status: i32) -> i8 {
+        ((status & 0xFF00) >> 8) as i8
+    }
+
+    fn signaled(status: i32) -> bool {
+        ((((status & 0x7f) + 1) as i8) >> 1) > 0
+    }
+
+    fn term_signal(status: i32) -> signal::SigNum {
+        (status & 0x7f) as signal::SigNum
+    }
+
+    fn dumped_core(status: i32) -> bool {
+        (status & 0x80) != 0
+    }
+
+    fn stopped(status: i32) -> bool {
+        (status & 0xff) == 0x7f
+    }
+
+    fn stop_signal(status: i32) -> signal::SigNum {
+        ((status & 0xFF00) >> 8) as signal::SigNum
+    }
+
+    fn continued(status: i32) -> bool {
+        status == 0xFFFF
+    }
+
+    pub fn decode(pid : pid_t, status: i32) -> WaitStatus {
+        if exited(status) {
+            WaitStatus::Exited(pid, exit_status(status))
+        } else if signaled(status) {
+            WaitStatus::Signaled(pid, term_signal(status), dumped_core(status))
+        } else if stopped(status) {
+            WaitStatus::Stopped(pid, stop_signal(status))
+        } else {
+            println!("status is {}", status);
+            assert!(continued(status));
+            WaitStatus::Continued(pid)
+        }
+    }
 }
 
 pub fn waitpid(pid: pid_t, options: Option<WaitPidFlag>) -> Result<WaitStatus> {
@@ -39,7 +96,7 @@ pub fn waitpid(pid: pid_t, options: Option<WaitPidFlag>) -> Result<WaitStatus> {
     } else if res == 0 {
         Ok(StillAlive)
     } else {
-        Ok(Exited(res))
+        Ok(status::decode(res, status))
     }
 }
 
