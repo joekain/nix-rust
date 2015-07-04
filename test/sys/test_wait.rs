cf658b0b6cdbadc0209a85b95336070a7c2542f3
Joseph Kain <joekain@gmail.com>
2015-07-04 12:12:22 -0700

    Wait for kill to complete

--- test/sys/test_wait.rs
+++ test/sys/test_wait.rs
@@ -9,8 +9,10 @@ fn test_wait_signal() {
     match fork() {
       Ok(Child) => {
           let pid = getpid();
+          assert!(pid > 0);
           kill(pid, SIGKILL).ok().expect("Error: Kill Failed");
-      }
+          loop {}
+      },
       Ok(Parent(_child_pid)) => {
           match wait() {
               Ok(WaitStatus::Signaled(_child_pid, SIGKILL, _dumped_core)) => {}  // success
@@ -25,9 +27,7 @@ fn test_wait_signal() {
 #[test]
 fn test_wait_exit() {
     match fork() {
-      Ok(Child) => {
-          unsafe { exit(12); }
-      }
+      Ok(Child) => unsafe { exit(12); },
       Ok(Parent(_child_pid)) => {
           let wait_status = wait();
           assert_eq!(wait_status, Ok(WaitStatus::Exited(_child_pid, 12)));
