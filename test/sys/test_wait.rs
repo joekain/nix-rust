4ae37a8b65d117fdf69fa709cac65b6f856273a0
Joseph Kain <joekain@gmail.com>
2015-07-04 12:51:23 -0700

    Fix wait tests
    * Use waitpid to make sure we wait for the right child.  Otherwise, if
      two tests run at the same time wait() could wake up for the wrong
      child.
    * In test_wait_signal kill from the parent and loop until killed in
      the child.

--- test/sys/test_wait.rs
+++ test/sys/test_wait.rs
@@ -7,17 +7,10 @@ use libc::funcs::c95::stdlib::exit;
 #[test]
 fn test_wait_signal() {
     match fork() {
-      Ok(Child) => {
-          let pid = getpid();
-          assert!(pid > 0);
-          kill(pid, SIGKILL).ok().expect("Error: Kill Failed");
-          loop {}
-      },
-      Ok(Parent(_child_pid)) => {
-          match wait() {
-              Ok(WaitStatus::Signaled(_child_pid, SIGKILL, _dumped_core)) => {}  // success
-              _ => panic!("Expected WaitStatus::Signaled")
-          }
+      Ok(Child) => loop { /* Wait for signal */ },
+      Ok(Parent(child_pid)) => {
+          kill(child_pid, SIGKILL).ok().expect("Error: Kill Failed");
+          assert_eq!(waitpid(child_pid, None), Ok(WaitStatus::Signaled(child_pid, SIGKILL, false)));
       },
       // panic, fork should never fail unless there is a serious problem with the OS
       Err(_) => panic!("Error: Fork Failed")
@@ -28,9 +21,8 @@ fn test_wait_signal() {
 fn test_wait_exit() {
     match fork() {
       Ok(Child) => unsafe { exit(12); },
-      Ok(Parent(_child_pid)) => {
-          let wait_status = wait();
-          assert_eq!(wait_status, Ok(WaitStatus::Exited(_child_pid, 12)));
+      Ok(Parent(child_pid)) => {
+          assert_eq!(waitpid(child_pid, None), Ok(WaitStatus::Exited(child_pid, 12)));
       },
       // panic, fork should never fail unless there is a serious problem with the OS
       Err(_) => panic!("Error: Fork Failed")
